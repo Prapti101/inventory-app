a8b5f5bea095f2e413ff109106f6fe922653d8e1
Prapti101 <praptipriya486@gmail.com>
2025-09-06 15:39:54 +0530

    Inital commit - inventory app

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,18 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-  const firebaseConfig = {
-  apiKey: "AIzaSyBwSXiLjmKqek1ClKh-XlG16aPlyo2fWv4",
-  authDomain: "inventory-app-db5f0.firebaseapp.com",
-  projectId: "inventory-app-db5f0",
-  storageBucket: "inventory-app-db5f0.firebasestorage.app",
-  messagingSenderId: "683468081937",
-  appId: "1:683468081937:web:a0cd70402be59e07c45ab0",
-  measurementId: "G-0D23JHDSXC"
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || ""
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
